fd0b458afc63429196f7841710455ecacaadbec9
EE-Sehaj-21104026 <96677133+EE-Sehaj-21104026@users.noreply.github.com>
2022-01-05 01:27:31 +0530

    Add files via upload

---
+++ A1 1.py
@@ -0,0 +1,6 @@
+# program for finding average 
+x= input("Enter first number: ")
+y= input("Enter second number: ")
+z= input("Enter third number: ")
+avg = (int(x)+int(y)+int(z))/3
+print("The average of three numbers is: ", avg)
